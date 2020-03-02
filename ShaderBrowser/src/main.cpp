d868f4fa61c530f6632370cfa10ca2be951c97fb
zhumeng <472434835@qq.com>
2020-03-02 10:42:36 +0800

    lightDir反了

--- ShaderBrowser/src/main.cpp
+++ ShaderBrowser/src/main.cpp
@@ -154,7 +154,7 @@ void testVal()
     // 设置平行光位置（没啥用）
     directionalLightEntity->setPosition(100, 100, 100);
     // 平行光方向
-    directionalLight->setDirection(-1, -1, 1);
+    directionalLight->setDirection(-1, -1, -1);
     // 平行光强度
     directionalLight->setIntensity(0.5f);
     // 平行光颜色
